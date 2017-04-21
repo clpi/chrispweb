3ad303d98bc3e41386ec9bdad48785e657f22c37
Chris Pecunies <chrisp1877@gmail.com>
2017-04-20 19:46:22 -0700

    processing

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var x;
+var y;
+function setup(){
+  var canvas = createCanvas(1500,800);
+  canvas.parent("container");
+  background(40);
+}
+function draw(){
+  x = map(mouseX, 0, 1500, 0, 255);
+  y = map(mouseY, 0, 800, 0, 255);
+  ellipse(mouseX,mouseY,50,50).fill(x,y,255,40).stroke(255,255,255,50);
+}
+function mousePressed(){
+  background(40);
+}
