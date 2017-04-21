f10317fa6d13f8a77d5eb7d3dd889bda1de5eb92
Chris Pecunies <chrisp1877@gmail.com>
2017-04-21 00:09:54 -0700

    gf

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup(){
 function draw(){
   x = map(mouseX, 0, 1500, 0, 255);
   y = map(mouseY, 0, 800, 0, 255);
-  ellipse(mouseX,mouseY,50,50).fill(x,y,255,40).stroke(255,255,255,50);
+  ellipse(mouseX,mouseY,60,60).fill(x,y,255,40).stroke(255,255,255,50);
 }
 function mousePressed(){
   background(40);
