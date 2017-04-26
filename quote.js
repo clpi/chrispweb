da6497994253be39dfbc9767584e7637b4245c31
Chris Pecunies <chrisp1877@gmail.com>
2017-04-26 00:08:02 -0700

    quote generator efficiency

--- quote.js
+++ quote.js
@@ -1,22 +1,29 @@
 var title;
 var author;
-function getQuote(){
-  var rand = Math.floor(Math.random() * 100);
+var quotes = [];
+function getData(){
   $.getJSON("https://www.reddit.com/r/showerthoughts/.json?limit=100&after=t3_10omtd/", function retrieve(data) {
        $.each(data.data.children.slice(0, 100), function (i,post) {
-         if(i === rand){
-           title = post.data.title;
-           author = post.data.author;
-           $("#quote").html( '<span style = "font-size: 20px"><br>"' + title
-           + '"</span><br><br><em> - ' + author + '</em>');
-         }
-        }
-      )
-    }
-  );
+         quotes.push([post.data.title,post.data.author]);
+       });
+       $("#quote").html( '<span style = "font-size: 20px"><br>"' + quotes[0][0]
+       + '"</span><br><br><em> - ' + quotes[0][1] + '</em>');
+  });
 }
-getQuote();
+function getQuote(){
+  var rand = Math.floor(Math.random() * 100);
+  for(var i=0;i<quotes.length;i++){
+    if(i === rand){
+      title = quotes[i][0];
+      author = quotes[i][1];
+      $("#quote").html( '<span style = "font-size: 20px"><br>"' + title
+      + '"</span><br><br><em> - ' + author + '</em>');
+    }
+  }
+}
 function tweet(){
   var address = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(title + ' \n' + author)
   window.open(address, '_blank');
 }
+getData();
+getQuote();
